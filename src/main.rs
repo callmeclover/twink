7a91f6e196b0203b76633cf9d57179b36474c020
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-09-10 23:17:51 -0400

    Switch to Rust
    (again)

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use adw::{prelude::*, AboutDialog, ActionRow, ApplicationWindow, HeaderBar};
+use gtk::{gio, Application, Box, ListBox, Orientation, ShortcutsWindow};
+
+fn main() {
+    let app: Application = Application::builder()
+        .application_id("com.github.callmeclover.Twink")
+        .build();
+
+    app.connect_startup(|_| {
+        adw::init().unwrap();
+    });
+    app.connect_activate(on_activate);
+
+    app.set_accels_for_action("win.about", &["<Ctrl>slash"]);
+    app.set_accels_for_action("win.show-help-overlay", &["<Ctrl>h"]);
+
+    app.run();
+}
+
+fn on_activate(app: &Application) {
+    // ActionRows are only available in Adwaita
+    let row: ActionRow = ActionRow::builder()
+        .activatable(true)
+        .selectable(false)
+        .title("Click me")
+        .build();
+    row.connect_activated(|_| {
+        eprintln!("Clicked!");
+    });
+
+    let list: ListBox = ListBox::builder()
+        .margin_top(32)
+        .margin_end(32)
+        .margin_bottom(32)
+        .margin_start(32)
+        // the content class makes the list look nicer
+        .css_classes(vec![String::from("content")])
+        .build();
+    list.append(&row);
+
+    // Combine the content in a box
+    let content: Box = Box::new(Orientation::Vertical, 0);
+    // Adwaitas' ApplicationWindow does not include a HeaderBar
+    content.append(
+        &HeaderBar::builder()
+            .title_widget(&adw::WindowTitle::new("Twink", "Nothing playing"))
+            .build(),
+    );
+    content.append(&list);
+
+    let window: ApplicationWindow = ApplicationWindow::builder()
+        .application(app)
+        .width_request(640)
+        .height_request(480)
+        .content(&content)
+        .show_menubar(true)
+        .build();
+
+    let about: AboutDialog = AboutDialog::builder()
+        .application_name("Twink")
+        .version("0.1.0")
+        .license_type(gtk::License::MitX11)
+        .license(include_str!("../LICENSE.md"))
+        .developer_name("Clover Johnson (callmeclover)")
+        .build();
+
+    let about_action: gio::ActionEntry<ApplicationWindow> = gio::ActionEntry::builder("about")
+        .activate(move |window: &ApplicationWindow, _, _| {
+            about.present(Some(window));
+        })
+        .build();
+
+    window.add_action_entries([about_action]);
+
+    let help: ShortcutsWindow = ShortcutsWindow::builder().application(app).transient_for(&window).build();
+    window.set_help_overlay(Some(&help));
+
+    window.present();
+}
